e928027a8b6acea4cf658a29f105ce91f4fc43cd
hanhalukmaria-bit <hanhalukmaria@gmail.com>
2026-06-16 15:53:15 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,22 +1,22 @@
+import os
 import re
 from telegram import Update
 from telegram.ext import Application, MessageHandler, filters, ContextTypes
 
-TOKEN = "8975717406:AAGssAo6nkZo65LZQosPxa6K2Ybi6iXd7wg"
+# токен берётся из Render (Environment Variables)
+TOKEN = os.getenv("TOKEN")
 
-# защита от зацикливания
 async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if update.message.from_user.is_bot:
         return
 
     text = update.message.text.lower()
 
-    # считаем все "мя" в тексте
+    # ищем все "мя"
     matches = re.findall(r'мя', text)
     count = len(matches)
 
     if count > 0:
-        # защита от слишком длинного спама
         if count > 30:
             count = 30
 
